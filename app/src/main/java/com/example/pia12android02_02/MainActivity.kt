506841d2277c96c0d777d72412b891bb949812fa
Mohamad Hleihel <mohammad.helehel@hotmail.com>
2024-02-02 11:44:47 +0100

    my own version

---
+++ app/src/main/java/com/example/pia12android02_02/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.pia12android02_02
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    var minsiffre = 0
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var mintext =findViewById<TextView>(R.id.Fancytext)
+        mintext.text = "banan"
+
+        var minknapp = findViewById<Button>(R.id.button)
+        minknapp.setOnClickListener {
+
+            minsiffre = minsiffre + 1
+
+            mintext.text = minsiffre.toString() }
+
+
+
+
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Pia12android02-02"
+include(":app")
